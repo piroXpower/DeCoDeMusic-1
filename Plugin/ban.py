926c23d537d07e749926a068c61e448aca2280a1
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2022-01-05 16:05:44 +0530

    Update ban.py

--- Plugin/ban.py
+++ Plugin/ban.py
@@ -4,11 +4,12 @@ from pyrogram import Client as bot
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from pyrogram import *
-from Assistant.helper.chatfilter import user_admin
+from helper.decorators import authorized_users_only
 # kick_chat_member
 
 
 @bot.on_message(filters.command("ban") & filters.group)
+@authorized_users_only
 def ban(bot, message):
     await bot.kick_chat_member(message.chat.id, message.reply_to_message.from_user.id)
     await bot.send_message(message.chat.id, f"{message.reply_to_message.from_user.mention} Banned Successfully")
@@ -18,7 +19,7 @@ try:
    
 # unban_chat_member
 @bot.on_message(filters.command("unban") & filters.group)
-
+@authorized_users_only
 def unban(bot, message):
     await bot.unban_chat_member(message.chat.id, message.reply_to_message.from_user.id)
     await bot.send_message(message.chat.id, f"{message.reply_to_message.from_user.mention} Unban Successfully")
