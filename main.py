ff2c199d2921b7eeee9cf3d767f033037aa74f7f
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2022-01-05 16:35:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,9 +4,6 @@ from pyrogram import Client as Bot
 
 from config import API_ID, API_HASH, BOT_TOKEN, BG_IMAGE
 
-response = requests.get(BG_IMAGE)
-with open("./etc/foreground.png", "wb") as file:
-    file.write(response.content)
 
 
 bot = Bot(
