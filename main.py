224a9e722a2a5fed64dbf18c1809847b85b0da4f
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2022-01-05 16:16:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import requests
 from pyrogram import idle
 from pyrogram import Client as Bot
 
-from Client.callsmusic import run
 from config import API_ID, API_HASH, BOT_TOKEN, BG_IMAGE
 
 response = requests.get(BG_IMAGE)
@@ -19,5 +18,4 @@ bot = Bot(
 )
 
 bot.start()
-run()
 idle() 
